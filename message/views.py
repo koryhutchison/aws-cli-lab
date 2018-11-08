47ff58b77a31ab689c54e50ad4237d926cfb86b7
Kory Hutchison <kory.hutchison@icloud.com>
2018-11-07 18:33:18 -0700

    Initial Commit

---
+++ message/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponse
+from django.template import loader
+from django import forms
+import boto3
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+def index(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SNSForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            if form.commit():
+                return HttpResponseRedirect('/message/success/')
+            else:
+                print("Failed to send to topic")
+                return HttpResponseRedirect('/message/')
+
+    template = loader.get_template('message/index.html')
+    return HttpResponse(template.render({}, request))
+
+def success(request):
+    template = loader.get_template('message/index.html')
+    return HttpResponse("Message Sent Successfully")
+
+class SNSForm(forms.Form):
+    message = forms.CharField(label='Message', max_length=100)
+
+    def commit(self):
+        try:
+            client = boto3.client(
+                "sns",
+                aws_access_key_id=settings.ACCESS_KEY,
+                aws_secret_access_key=settings.SECRET_KEY,
+                region_name="us-east-1"
+            )
+
+            client.publish(
+                TopicArn=settings.TOPIC_ARN,
+                Message=self.cleaned_data.get('message')
+            )
+        except:
+            return False
+
+        return True
